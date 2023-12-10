d8e8ba9998330388fc7b78337bd06839a2146498
wintrrnub <152836817+wintrrnub@users.noreply.github.com>
2023-12-10 20:46:06 +0700

    Add files via upload

---
+++ assets/js/meyawo.js
@@ -0,0 +1,53 @@
+/*!
+=========================================================
+* Meyawo Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function () {
+    $(".navbar .nav-link").on('click', function (event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+// navbar toggle
+$('#nav-toggle').click(function () {
+    $(this).toggleClass('is-active')
+    $('ul.nav').toggleClass('show');
+});
+
+google.charts.load("current", { packages: ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ["Element", "Density", { role: "style" }],
+        ["Copper", 8.94, "#b87333"],
+        ["Silver", 10.49, "silver"],
+        ["Gold", 19.30, "gold"],
+        ["Platinum", 21.45, "color: #e5e4e2"]
+    ]);
+    var view = new google.visualization.DataView(data); view.setColumns([0, 1, { calc: "stringify", sourceColumn: 1, type: "string", role: "annotation" }, 2]);
+    var options = { title: "Density of Precious Metals, in g/cm^3", width: 600, height: 400, bar: { groupWidth: "95%" }, legend: { position: "none" }, };
+    var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values")); chart.draw(view, options);
+}
